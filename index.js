b9c60b2921fe4b9006babc59769930ca10727b5b
saquibaijaz <saquibaijaz@navgurukul.org>
2023-03-13 19:38:38 +0530

    Added validation for empty username and fixed undefined message

--- index.js
+++ index.js
@@ -3,26 +3,29 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.ejs');
 });
 
-io.sockets.on('connection', function(socket) {
-    socket.on('username', function(username) {
-        socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+io.sockets.on('connection', function (socket) {
+    socket.on('username', function (username) {
+        if (username) { // check if username is not null or empty
+            socket.username = username;
+            io.emit('is_online', '🔵 <i>' + socket.username + ' joined the chat..</i>');
+        }
     });
 
-    socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    socket.on('disconnect', function (username) {
+        if (socket.username) { // check if username is not null or empty
+            io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+        }
     })
 
-    socket.on('chat_message', function(message) {
+    socket.on('chat_message', function (message) {
         io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
     });
-
 });
 
-const server = http.listen(8080, function() {
+const server = http.listen(8080, function () {
     console.log('listening on *:8080');
 });
